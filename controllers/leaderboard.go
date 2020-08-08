56d81d4fb501c51836bfa88e61d7a66c3c5c0cd5
Ash Midgley <ayelgdom@tutanota.com>
2020-08-08 13:27:07 +0100

    Initial wire up.

--- controllers/leaderboard.go
+++ controllers/leaderboard.go
@@ -7,30 +7,64 @@ import (
     "io/ioutil"
     "encoding/json"
     "strconv"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
     "../models"
 )
-
-var entries = []models.Entry{}
 
 func GetEntry(writer http.ResponseWriter, request *http.Request) {
     id, err := strconv.Atoi(mux.Vars(request)["id"])
     if(err != nil) {
         fmt.Fprintf(writer, "Id value is not an int.")
-        return
     }
 
-    for _, value := range entries {
-        if value.Id == id {
-            json.NewEncoder(writer).Encode(value)
-            return
-        }
+    database, err := sql.Open("sqlite3", "./leaderboard.db")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer database.Close()
+
+    statement, err := database.Prepare("SELECT * FROM leaderboard WHERE id = ?")
+	if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer statement.Close()
+
+    var entry models.Entry
+    err = statement.QueryRow(id).Scan(&entry)
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
     }
 
-    writer.WriteHeader(http.StatusNotFound)
+    json.NewEncoder(writer).Encode(entry)
 }
 
 func GetEntries(writer http.ResponseWriter, request *http.Request) {
-    json.NewEncoder(writer).Encode(entries)
+    database, err := sql.Open("sqlite3", "./leaderboard.db")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer database.Close()
+
+    rows, err := database.Query("SELECT * FROM leaderboard")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var id int
+        var name string
+        var country string
+        var countries int
+        var time string
+        err = rows.Scan(&id, &name, &country, &countries, &time)
+        if err != nil {
+            fmt.Fprintf(writer, err.Error())
+        }
+    }
+
+    json.NewEncoder(writer).Encode(rows)
 }
 
 func CreateEntry(writer http.ResponseWriter, request *http.Request) {
@@ -41,7 +75,21 @@ func CreateEntry(writer http.ResponseWriter, request *http.Request) {
 
     var newEntry models.Entry
     json.Unmarshal(requestBody, &newEntry)
-    entries = append(entries, newEntry)
+
+    database, err := sql.Open("sqlite3", "./leaderboard.db")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer database.Close()
+
+    statement, err := database.Prepare("INSERT into leaderboard (name, country, countries, time) values (?, ?, ?, ?)")
+	if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer statement.Close()
+
+    statement.Exec(newEntry.Name, newEntry.Country, newEntry.Countries, newEntry.Time)
+
     writer.WriteHeader(http.StatusCreated)
     json.NewEncoder(writer).Encode(newEntry)
 }
@@ -50,48 +98,52 @@ func UpdateEntry(writer http.ResponseWriter, request *http.Request) {
     id, parseErr := strconv.Atoi(mux.Vars(request)["id"])
     if(parseErr != nil) {
         fmt.Fprintf(writer, "Id value is not an int.")
-        return
     }
 
     requestBody, readErr := ioutil.ReadAll(request.Body)
     if readErr != nil {
         fmt.Fprintf(writer, "Request body invalid.")
-        return
     }
 
     var updatedEntry models.Entry
     json.Unmarshal(requestBody, &updatedEntry)
 
-    for i, value := range entries {
-        if value.Id == id {
-            value.Name = updatedEntry.Name
-            value.Country = updatedEntry.Country
-            value.Countries = updatedEntry.Countries
-            value.Time = updatedEntry.Time
-            entries = append(entries[:i], value)
-            json.NewEncoder(writer).Encode(value)
-            return
-        }
-    }
+    database, err := sql.Open("sqlite3", "./leaderboard.db")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer database.Close()
 
-    writer.WriteHeader(http.StatusNotFound)
+    statement, err := database.Prepare("UPDATE leaderboard set name = ?, country = ?, countries = ?, time = ? where id = ?")
+	if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer statement.Close()
+
+    statement.Exec(updatedEntry.Name, updatedEntry.Country, updatedEntry.Countries, updatedEntry.Time, id)
+
+    updatedEntry.Id = id
+    json.NewEncoder(writer).Encode(updatedEntry)
 }
 
 func DeleteEntry(writer http.ResponseWriter, request *http.Request) {
     id, err := strconv.Atoi(mux.Vars(request)["id"])
     if(err != nil) {
         fmt.Fprintf(writer, "Id value is not an int.")
-        return
     }
 
-    for i, value := range entries {
-        if value.Id == id {
-            entries = append(entries[:i], entries[i+1:]...)
-            json.NewEncoder(writer).Encode(value)
-            return
-        }
-    }
+    database, err := sql.Open("sqlite3", "./leaderboard.db")
+    if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer database.Close()
 
-    writer.WriteHeader(http.StatusNotFound)
+    statement, err := database.Prepare("DELETE FROM leaderboard WHERE id = ?")
+	if err != nil {
+        fmt.Fprintf(writer, err.Error())
+	}
+    defer statement.Close()
+
+    statement.Exec(id)
 }
 
