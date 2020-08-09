f32d0de9278de144119b186dda54d71011ec3251
Ash Midgley <ayelgdom@tutanota.com>
2020-08-09 10:26:14 +0100

    Squash to avoid modules hehe.

--- models/entry.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Entry struct {
-    Id int `json:"id"`
-    Name string `json:"name"`
-    Country string `json:"country"`
-    Countries int `json:"countries"`
-    Time string `json:"time"`
-}
