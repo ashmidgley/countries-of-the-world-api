d81e3261b81b4add7b9415ec630cd7b69f46abad
Ash Midgley <ayelgdom@tutanota.com>
2020-08-09 17:55:11 +0100

    Fix up codes.

--- scripts/codes.py
+++ scripts/codes.py
@@ -9,17 +9,11 @@ def codes_to_dictionary(codes):
         dict[key] = value
     return dict;
 
-def print_codes(codes):
-    for x in codes:
-        line = x.strip().split('"')
-        key = line[3]
-        value = line[1].lower()
-        print('"' + key + '": "' + value + '",') 
-
 if __name__ == "__main__":
     filename = "codes.json"
     with open(filename) as f:
         codes = f.readlines()
     codes = codes[1:-1]
     dict = codes_to_dictionary(codes);
-    #print_codes(codes)
+    for (key, value) in sorted(dict.items()):
+        print('"' + key + '": "' + value + '",') 
