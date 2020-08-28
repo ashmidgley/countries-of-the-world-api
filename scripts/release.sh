23d632cb959179e955403aaf925a8fd52029c100
ash.midgley <ash.midgley@thecomplianceworkbook.com>
2020-08-28 17:00:15 +0100

    Move database to seperate folder.

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,4 +7,4 @@ docker rm cotw-api
 echo "##### Building new image"
 docker build -t cotw-api ../
 echo "##### Starting new API container"
-docker run -d -p 8080:8080 -v countries-of-the-world:/go/src/app --name cotw-api cotw-api
+docker run -d -p 8080:8080 -v countries-of-the-world:/go/src/app/database --name cotw-api cotw-api
